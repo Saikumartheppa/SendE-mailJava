7037ce7ec622c889aef6154b6ef0e60bf06a099b
Saikumartheppa <saikumartheppa06@gmail.com>
2023-06-11 14:45:33 +0530

    created by Saikumar

---
+++ SendMailJava/src/main/java/org/example/MailMetaData.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class MailMetaData {
+    public static final String HostServer = "smtp.gmail.com";
+    public static final String port = "465";
+    public static final String sslProperty = "mail.smtp.ssl.enable";
+    public static final String authPerm = "mail.smtp.auth";
+
+    public static final String myUserMail = "saikumartheppa06@gmail.com";
+
+    public static final String password = "hjhlbobhfybgmffj";// app password
+
+    public static final String receiverMail = "chintooo1436@gmail.com";
+}
